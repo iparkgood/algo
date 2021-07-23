4774145d81cf2847476a92385c60d3f5091dd31c
iparkgood <zuzu8723@gmail.com>
2021-07-23 14:29:18 -0400

    refactored getNthFib

--- Easy/getNthFib.js
+++ Easy/getNthFib.js
@@ -1,15 +1,21 @@
 function getNthFib(n) {
   let fibos = [0, 1];
+  let counter = 3;
 
   if (n === 1) {
     return 0;
   } else if (n === 2) {
     return 1;
   } else {
-    for (let i = 2; i < n; i++) {
-      fibos.push(fibos[i - 1] + fibos[i - 2]);
+    while (counter <= n) {
+      const next = fibos[0] + fibos[1];
+      fibos[0] = fibos[1];
+      fibos[1] = next;
+      counter++;
     }
   }
 
-  return fibos[fibos.length - 1];
+  return fibos[1];
 }
+
+console.log(getNthFib(9));
